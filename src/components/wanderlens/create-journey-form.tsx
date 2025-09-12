2405f17965641909ac624b16debfdb676d225917
smartindia innovasions <smartindiainnovasions@gmail.com>
2025-09-12 06:41:57 +0000

    implement this icon for auto

--- src/components/wanderlens/create-journey-form.tsx
+++ src/components/wanderlens/create-journey-form.tsx
@@ -43,23 +43,23 @@ const currencies = [
 
 const AutoRickshawIcon = (props: React.SVGProps<SVGSVGElement>) => (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        {...props}
     >
-        <path d="M14 18V6a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8"/>
-        <path d="M6 18h2.3a1 1 0 0 0 .9-.6L11 14"/>
-        <path d="m11 14-2.5 4"/>
-        <path d="M14 18h4"/>
-        <path d="M19 18a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
-        <path d="M4.5 10H10"/>
+        <path d="M4 19a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2Z"/>
+        <path d="m10 5-3 5"/>
+        <path d="M10 19h8"/>
+        <path d="M18 19a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
+        <path d="m6 9-3 4"/>
+        <path d="M3 13h1"/>
     </svg>
 );
 
